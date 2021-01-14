68ca62c6af6f32481229bd66b75e1d029fcce16d
akbarakma <akbarajo1234@gmail.com>
2021-01-14 21:06:20 +0700

    Add vendor router

---
+++ server/controllers/VendorController.js
@@ -0,0 +1,29 @@
+const {
+  vendor
+} = require("../models");
+
+class VendorController {
+  static getAll = async (req, res, next) => {
+    try {
+      let { page } = req.query;
+      if (!page || page < 1) page = 1;
+      const resPerPage = 15;
+      const offset = (resPerPage * page) - resPerPage;
+      const result = await vendor.findAll({
+        limit: resPerPage,
+        offset
+      });
+      const numOfResult = await vendor.count();
+      res.status(200).json({
+        data: result,
+        pages: Math.ceil(numOfResult / resPerPage),
+        currentPage: Number(page),
+        numOfResult
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+};
+
+module.exports = VendorController;
--- server/routers/index.js
+++ server/routers/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const users = require("./users");
+const vendors = require("./vendors");
 
 router.use("/users", users);
+router.use("/vendors", vendors);
 
 module.exports = router;
