b87549b6179593d0ccff668188f08bb203b6c941
Indra Puji Novirwan <indrapuji@gmail.com>
2021-02-23 03:16:23 +0700

    new detail

--- client/src/views/pages/workorder/DetailWorkorders.js
+++ client/src/views/pages/workorder/DetailWorkorders.js
@@ -5,7 +5,7 @@ import {
   CRow,
   CCol,
   CTextarea,
-  CSelect,
+  // CSelect,
   CInput,
   CFormGroup,
   CForm,
@@ -23,17 +23,24 @@ import axios from 'axios';
 import HostUrl from '../../../components/HostUrl';
 import newAlert from '../../../components/NewAlert';
 import Swal from 'sweetalert2';
+import { formatDate, formatTime } from 'node-format-date';
 
 const Register = () => {
   const { woId } = useParams();
   const history = useHistory();
   // const [teknisiList, setTeknisiList] = useState(null);
   const [detail, setDetail] = useState({});
+  const [nama, setNama] = useState('');
 
   useEffect(() => {
     getDetailWO();
     // eslint-disable-next-line
   }, []);
+
+  useEffect(() => {
+    getUsers();
+    // eslint-disable-next-line
+  }, [detail]);
 
   const getDetailWO = async () => {
     try {
@@ -45,8 +52,24 @@ const Register = () => {
           token,
         },
       });
+
       console.log(data);
       setDetail(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getUsers = async () => {
+    try {
+      const { data } = await axios({
+        method: 'GET',
+        url: HostUrl + '/users/single/' + detail.teknisi_id,
+        headers: {
+          token: localStorage.getItem('token'),
+        },
+      });
+      setNama(data.nama_lengkap);
     } catch (error) {
       console.log(error);
     }
@@ -89,106 +112,144 @@ const Register = () => {
       <CRow className="justify-content-center">
         <CCol xs="12" md="12">
           <CCard>
-            <CCardHeader color={detail.tipe}>Detail Merchant</CCardHeader>
+            <CCardHeader color={detail.tipe}>
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <div>Detail Merchant</div>
+                <div>{detail.tipe}</div>
+              </div>
+            </CCardHeader>
             <CCardBody>
               <CForm>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                  <div>
+                    Tanggal Import
+                    <div>{formatTime(detail.createdAt)}</div>
+                    <p>{formatDate(detail.createdAt)}</p>
+                  </div>
+                  <div>
+                    Tanggal Assign
+                    <div>{detail.tanggal_assign ? formatTime(detail.tanggal_assign) : '-'}</div>
+                    <p>{detail.tanggal_assign ? formatDate(detail.tanggal_assign) : ''}</p>
+                  </div>
+                  <div>
+                    Tanggal Done
+                    <div>{detail.tanggal_done ? formatTime(detail.tanggal_done) : '-'}</div>
+                    <p>{detail.tanggal_done ? formatDate(detail.tanggal_done) : ''}</p>
+                  </div>
+                </div>
                 <CFormGroup row>
                   <CCol md="2">
-                    <CLabel htmlFor="text-input">Nama Merchant</CLabel>
+                    <CLabel>Assign</CLabel>
                   </CCol>
-                  <CCol xs="12" md="5">
-                    <CLabel>{detail.merchant}</CLabel>
+                  <CCol xs="12" md="9">
+                    <CInput size="sm" style={{ backgroundColor: !nama ? 'red' : null }} value={nama} disabled />
                   </CCol>
-                  <CCol xs="12" md="5">
+                </CFormGroup>
+                <CFormGroup row>
+                  <CCol md="2">
+                    <CLabel>Nama Merchant</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                    <CInput size="sm" value={detail.merchant} disabled />
+                  </CCol>
+                </CFormGroup>
+                {/* <CFormGroup row>
+                  <CCol md="2" />
+                  <CCol xs="12" md="9">
                     <CInput size="sm" value={detail.nama_merchant} />
                   </CCol>
-                </CFormGroup>
-
+                </CFormGroup> */}
                 <CFormGroup row>
                   <CCol md="2">
-                    <CLabel htmlFor="text-input">Alamat Merchant</CLabel>
-                  </CCol>
-                  <CCol xs="12" md="5">
-                    <CTextarea name="alamat" />
-                  </CCol>
-                </CFormGroup>
-                <CFormGroup row>
-                  <CCol md="2">
-                    <CLabel htmlFor="text-input">No Telp</CLabel>
+                    <CLabel>Alamat Merchant</CLabel>
                   </CCol>
                   <CCol xs="12" md="9">
-                    <CInput placeholder="Masukkan Telepon Merchant..." name="no_telp" />
+                    <CTextarea value={detail.alamat} name="alamat" disabled />
+                  </CCol>
+                </CFormGroup>
+                {/* <CFormGroup row>
+                  <CCol md="2" />
+                  <CCol xs="12" md="9">
+                    <CTextarea value={detail.alamat_merchant} name="alamat" />
+                  </CCol>
+                </CFormGroup> */}
+                <CFormGroup row>
+                  <CCol md="2">
+                    <CLabel>No Telp</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                    <CInput value={detail.no_telp} name="no_telp" disabled />
+                  </CCol>
+                </CFormGroup>
+                {/* <CFormGroup row>
+                  <CCol md="2" />
+                  <CCol xs="12" md="9">
+                    <CInput name="no_telp_merchant" />
+                  </CCol>
+                </CFormGroup> */}
+                <CFormGroup row>
+                  <CCol md="2">
+                    <CLabel>Kota</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                    <CInput value={detail.kota} disabled name="kota" />
                   </CCol>
                 </CFormGroup>
                 <CFormGroup row>
                   <CCol md="2">
-                    <CLabel htmlFor="text-input">Kota</CLabel>
+                    <CLabel>MID</CLabel>
                   </CCol>
                   <CCol xs="12" md="9">
-                    <CInput placeholder="Masukkan Kota..." name="kota" />
+                    <CInput value={detail.mid} name="mid" disabled />
                   </CCol>
                 </CFormGroup>
                 <CFormGroup row>
                   <CCol md="2">
-                    <CLabel htmlFor="text-input">MID</CLabel>
+                    <CLabel>TID</CLabel>
                   </CCol>
                   <CCol xs="12" md="9">
-                    <CInput placeholder="Masukkan Serial Number..." name="mid" />
+                    <CInput value={detail.tid} name="tid" disabled />
                   </CCol>
                 </CFormGroup>
                 <CFormGroup row>
                   <CCol md="2">
-                    <CLabel htmlFor="text-input">TID</CLabel>
+                    <CLabel>EDC Connection</CLabel>
                   </CCol>
                   <CCol xs="12" md="9">
-                    <CInput placeholder="Masukkan Serial Number..." name="tid" />
+                    <CInput value={detail.edc_connection} disabled name="edc_connection" />
                   </CCol>
                 </CFormGroup>
                 <CFormGroup row>
                   <CCol md="2">
-                    <CLabel htmlFor="text-input">EDC Connection</CLabel>
+                    <CLabel>Tipe EDC</CLabel>
                   </CCol>
                   <CCol xs="12" md="9">
-                    <CInput placeholder="Masukkan EDC Connection..." name="edc_connection" />
+                    <CInput value={detail.type_edc} disabled name="type_edc" />
                   </CCol>
                 </CFormGroup>
                 <CFormGroup row>
                   <CCol md="2">
-                    <CLabel htmlFor="text-input">Tipe EDC</CLabel>
+                    <CLabel>Regional</CLabel>
                   </CCol>
                   <CCol xs="12" md="9">
-                    <CInput placeholder="Masukkan EDC Connection..." name="type_edc" />
+                    <CInput value={detail.regional} disabled name="regional" />
                   </CCol>
                 </CFormGroup>
                 <CFormGroup row>
                   <CCol md="2">
-                    <CLabel htmlFor="text-input">Regional</CLabel>
+                    <CLabel>Catatan</CLabel>
                   </CCol>
                   <CCol xs="12" md="9">
-                    <CInput placeholder="Masukkan EDC Connection..." name="regional" />
-                  </CCol>
-                </CFormGroup>
-                <CFormGroup row>
-                  <CCol md="2">
-                    <CLabel htmlFor="select">Tipe</CLabel>
-                  </CCol>
-                  <CCol xs="12" md="9">
-                    <CSelect custom id="select" name="tipe">
-                      <option value="0">Please select</option>
-                      <option value="Kunjungan">Kunjungan</option>
-                      <option value="Pickup">Pickup Sales Draft</option>
-                      <option value="Survey">OTS Survey</option>
-                      <option value="Risk">Risk Unit</option>
-                    </CSelect>
+                    <CInput value={detail.catatan} disabled />
                   </CCol>
                 </CFormGroup>
               </CForm>
             </CCardBody>
             <CCardFooter>
-              <CButton type="submit" size="sm" color="warning" onClick={handleDelete}>
+              <CButton size="sm" color="warning" onClick={handleDelete}>
                 <CIcon name="cil-scrubber" /> Delete
               </CButton>
-              <CButton type="submit" size="sm" className="float-right" color="success">
+              <CButton size="sm" className="float-right" color="success">
                 <CIcon name="cil-scrubber" /> Close
               </CButton>
             </CCardFooter>
